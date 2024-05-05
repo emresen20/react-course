db2b044a38f5f6b9c959709a159345f2d14d7e5e
emresen20 <esen20@posta.pau.edu.tr>
2024-05-05 18:53:43 +0300

    instalaation complete and props learnd

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Texts from "./components/Texts"
+import Home from "./pages/Home"
+
+
+
+
+function App() {
+
+  return (
+    <>
+      <div >
+        react eğitimi1
+      </div>
+      <div >
+        react eğitimi2
+      </div>
+      <div >
+        react eğitimi3
+      </div>
+      <Texts text={"selam ben props"}/>
+     
+    </>
+
+  )
+}
+
+export default App
---
+++ src/components/Texts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Texts = ({text}) => {
+  return (
+    <div>
+      {text}
+    </div>
+  )
+}
+
+export default Texts
