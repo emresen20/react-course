2e7ea2f2240cec50379c6fff001b38f4917b3a91
emresen20 <esen20@posta.pau.edu.tr>
2024-05-17 19:05:26 +0300

    Put isteği güncellemek için kullanılır

--- uygulama/apiistek/apiistek/src/App.jsx
+++ uygulama/apiistek/apiistek/src/App.jsx
@@ -13,15 +13,25 @@ const App = () => {
    console.log(response.data)
   }
 
+  // useEffect(()=>{
+  //   const newUser={
+  //       "username": "Kemal",
+  //       "password": "123123",
+
+  //   }
+  //   createUser(newUser)
+    
+  // },[])
+
   useEffect(()=>{
     const newUser={
-        "username": "Kemal",
-        "password": "123123",
-
+             "username": "Ali",
+            "password": "12312dddd3",
     }
-    createUser(newUser)
-    
-  },[])
+
+    updateUser("af2d",newUser)
+
+  })
 
   const getUSerbyId= async (userId)=>{
     const response = await axios.get(`${BASE_URL}/users/${userId}`)
@@ -31,6 +41,11 @@ const App = () => {
   const createUser= async (newUser)=>{
   const response=  await axios.post(`${BASE_URL}/users`,newUser)
     console.log("response",response.data)
+  }
+
+  const updateUser = async(userId,uptaedUser)=>{
+    // Put veri güncellleme için kullanulıur
+  await  axios.put(`${BASE_URL}/users/${userId}`,uptaedUser);
   }
 
   return (
